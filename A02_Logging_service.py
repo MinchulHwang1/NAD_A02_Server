430a3d3f15e65072154e224fc080d37085c67307
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-02-23 13:17:06 -0500

    Save comments and delete UDP code.
    I wrote down the comments of log files
    and delete UDP code to specify TCP code.

--- A02_Logging_service.py
+++ A02_Logging_service.py
@@ -1,10 +1,24 @@
+# File name         : A02_Logging_service.py
+# Programmer        : Minchul Hwang - 8818858
+# Submission date   : 2/24/2024
+# Purpose           : This program is service of logging.
+#                     and it can take messages from client which is made by C++ file,
+#                     the messages has some kind of information like command, file name, IP etc.
+#                     this program distinguish that messages and translate them to print in log file.
+
 import socket
 import datetime
 import threading
 
+# Function header comment.
+# function Name       : clientHandler
+# Purpose             : this function can distinguish the messages and print properly messages in log file
+# Input               : connection        the information what is come from client
+#                       address           the IP and port address
+# Output              : the input messages in log file 
+# Return              : NONE
 def clientHandler(connection, address):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    connected_flag = False
 
     try:
         
@@ -38,6 +52,14 @@ def clientHandler(connection, address):
         print(f"Connection from {address} (Client {number}) closed.")
 
 
+# Function header comment.
+# function Name       : receive_TCP_message
+# Purpose             : This function can figure oout the information of IP and port.
+#                       if the server is connected, the message is written down in logfile
+# Input               : ip              the IP address
+#                       port            the port of the IP
+# Output              : the input messages in log file 
+# Return              : NONE
 def receive_TCP_message(ip, port):
 # Create TCP socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -67,6 +89,11 @@ def receive_TCP_message(ip, port):
 
 
 
+# MAIN Function header comment.
+# Purpose             : Save IP, port, and send them to receive_TCP_message function
+# Input               : NONE
+# Output              : NONE
+# Return              : NONE
 if __name__ == "__main__":
     ip = '10.169.92.221'  # the ip address which client sent
     port = 24  # the port number which client sent
@@ -75,58 +102,3 @@ if __name__ == "__main__":
 
 
 
-
-
-# def receive_udp_message(ip, port):
-#     # Create UDP socket
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-#     # Binding ip and port to address
-#     server_address = (ip, port)
-#     sock.bind(server_address)
-
-#     print("UDP Opened.")
-#     with open("logfile.txt", "a") as logfile:
-#         logfile.write(f"{current_time} - Logging service started. Waiting for connections...\n")
-
-
-#     try:
-#         # waiting
-#         while True:
-#             print("\nwaiting...")
-#             data, address = sock.recvfrom(4096)  
-            
-            
-#             print(f"received message: {data.decode()}")
-#             received_message = data.decode()
-#             parts = received_message.split(maxsplit=2)
-#             if len(parts) == 3:
-#                 number ,command, file_name = parts
-#                 if command == "-o":
-#                     with open("logfile.txt", "a") as logfile:
-#                         logfile.write(f"{current_time} - {number} - {file_name} opened.\n")
-#                 elif command == "-c":
-#                     with open("logfile.txt", "a") as logfile:
-#                         logfile.write(f"{current_time} - {file_name} closed.\n")
-#                 elif command == "-r":
-#                     with open("logfile.txt", "a") as logfile:
-#                         logfile.write(f"{current_time} - {file_name} is open for read.\n")
-#                 elif command == "-w":
-#                     with open("logfile.txt", "a") as logfile:
-#                         logfile.write(f"{current_time} - {file_name} modified.\n")
-#                 else :
-#                     with open("logfile.txt", "a") as logfile:
-#                         logfile.write(f"{current_time} - Wrong command is sent.\n")
-#     finally:
-#         sock.close()
-
-# if __name__ == "__main__":
-#     ip = '10.169.92.221'  # the ip address which client sent
-#     port = 24  # the port number which client sent
-
-#     receive_udp_message(ip, port)
-
-
-# with open("logfile.txt", "a") as logfile:
-#                logfile.write(f"{current_time} - Connected.\n")
